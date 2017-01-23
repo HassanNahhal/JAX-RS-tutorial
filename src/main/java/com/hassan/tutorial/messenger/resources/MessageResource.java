14c3c295bd1efd49e675d74db009a869254aa209
HassanNahhal <hassannahhal@Hassans-MacBook-Pro.local>
2017-01-22 22:09:53 -0500

    Add POST method

--- src/main/java/com/hassan/tutorial/messenger/resources/MessageResource.java
+++ src/main/java/com/hassan/tutorial/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package com.hassan.tutorial.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,6 +33,12 @@ public class MessageResource {
     return messageService.getMessage(id);
   }
 
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Message addMessage(Message message) {
+    return messageService.addMessage(message);
+  }
 
 
 }
