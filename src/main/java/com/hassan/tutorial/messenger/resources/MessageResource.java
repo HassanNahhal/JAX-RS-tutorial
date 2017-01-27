c6c1191d74fbf314007241087f40170b91b97b4c
HassanNahhal <hassannahhal@Hassans-MacBook-Pro.local>
2017-01-27 15:01:22 -0500

    Add filtering and pagination
    • Filter based on year
    • Paginated with starting element and size

--- src/main/java/com/hassan/tutorial/messenger/resources/MessageResource.java
+++ src/main/java/com/hassan/tutorial/messenger/resources/MessageResource.java
@@ -10,50 +10,52 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.hassan.tutorial.messenger.model.Message;
 import com.hassan.tutorial.messenger.service.MessageService;
 
-
 @Path("/messages")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
-
-
-  MessageService messageService = new MessageService();
-
-
-  @PUT
-  @Path("/{messageId}")
-  public Message updateMessage(@PathParam("messageId") long id, Message message) {
-    message.setId(id);
-    return messageService.updateMessage(message);
-  }
-
-
-  @DELETE
-  @Path("/{messageId}")
-  public void deleteMessage(@PathParam("messageId") long id) {
-    messageService.removeMessage(id);
-  }
-
-  @GET
-  public List<Message> getMessages() {
-    return messageService.getAllMessages();
-  }
-
-  @GET
-  @Path("/{messageId}")
-  public Message getMessage(@PathParam("messageId") long id) {
-    return messageService.getMessage(id);
-  }
-
-  @POST
-  public Message addMessage(Message message) {
-    return messageService.addMessage(message);
-  }
-
-
+     
+     MessageService messageService = new MessageService();
+     
+     @GET
+     public List<Message> getMessages(@QueryParam("year") int year,
+                                      @QueryParam("start") int start,
+                                      @QueryParam("size") int size) {
+          if (year > 0)
+               return messageService.getAllMessagesForYear(year);
+          if (start >= 0 && size >= 0)
+               return messageService.getAllMessagesPaginated(start, size);
+          return messageService.getAllMessages();
+     }
+     
+     @PUT
+     @Path("/{messageId}")
+     public Message updateMessage(@PathParam("messageId") long id,
+                                  Message message) {
+          message.setId(id);
+          return messageService.updateMessage(message);
+     }
+     
+     @DELETE
+     @Path("/{messageId}")
+     public void deleteMessage(@PathParam("messageId") long id) {
+          messageService.removeMessage(id);
+     }
+     
+     @GET
+     @Path("/{messageId}")
+     public Message getMessage(@PathParam("messageId") long id) {
+          return messageService.getMessage(id);
+     }
+     
+     @POST
+     public Message addMessage(Message message) {
+          return messageService.addMessage(message);
+     }
 }
